365e8c66214b86edf77c87c017f8aa81daaf713e
MrBlackRabbit <mr.bl4ck.rabbit@gmail.com>
2024-06-07 19:35:36 +0300

    update

--- src/main/java/by/itclass/MySpringBootApp.java
+++ src/main/java/by/itclass/MySpringBootApp.java
@@ -1,2 +1,19 @@
-package by.itclass;public class MySpringBootApp {
+package by.itclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class MySpringBootApp {
+    @GetMapping
+    String home() {
+        return "Hello World!!!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApp.class);
+    }
 }
